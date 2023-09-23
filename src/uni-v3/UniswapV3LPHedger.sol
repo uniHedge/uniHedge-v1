2fa9cf1267efa08b629d8a9a35a3e4bac414d980
riemann <aleqvids@gmail.com>
2023-09-23 08:11:19 -0400

    external => public

--- src/uni-v3/UniswapV3LPHedger.sol
+++ src/uni-v3/UniswapV3LPHedger.sol
@@ -22,7 +22,7 @@ contract UniswapV3LPHedger is UniswapV3LiquidityProvider, Leverage {
     function openHedgedLP(address token0, address token1, int24 tickLower, int24 tickUpper) external {
 
         // @dev todo: calculate the amount to supply as LP to uni, and to short
-        mintNewPosition(token0, token1, 1e18, 1e18); // filler vals
+        mintNewPosition(token0, token1, 1e18, 1e18); // filler vals`
         leverage.short(token0, token1, 1e18, ud(1e18));
     }
 
