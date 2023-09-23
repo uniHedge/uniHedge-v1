607df82aaf8d846be75d6f9b74915969e3bb5fc8
riemann <aleqvids@gmail.com>
2023-09-22 21:12:06 -0400

    update env

--- src/Provider.sol
+++ src/Provider.sol
@@ -86,7 +86,6 @@ contract UniswapV3Liquidity is IERC721Receiver {
     ) external returns (uint128 liquidity, uint amount0, uint amount1) {
 
          // (uint96 nonce, address operator, address token0, address token1, uint24 fee, int24 tickLower, int24 tickUpper, uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, uint128 tokensOwed0, uint128 tokensOwed1)
-
         (, , address token0, address token1, , , , , , , , ) = nonfungiblePositionManager.positions(tokenId);
 
         IERC20(token0).transferFrom(msg.sender, address(this), amount0ToAdd);
