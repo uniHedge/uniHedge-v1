148f0418904367dde7e512fc73fac3379f111f31
riemann <aleqvids@gmail.com>
2023-09-23 00:25:37 -0400

    edits

--- src/uni-v3/interfaces/interfaces.sol
+++ src/uni-v3/interfaces/interfaces.sol
@@ -72,34 +72,14 @@ interface INonfungiblePositionManager {
     uint256 tokenId
   ) external view returns (uint96 nonce, address operator, address token0, address token1, uint24 fee, int24 tickLower, int24 tickUpper, uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, uint128 tokensOwed0, uint128 tokensOwed1);
 }
-/* 
-interface IERC20 {
-    function totalSupply() external view returns (uint);
 
-    function balanceOf(address account) external view returns (uint);
 
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
-
-interface IWETH is IERC20 {
+interface IWETH {
     function deposit() external payable;
 
     function withdraw(uint amount) external;
 }
- */
+
 
 interface IPOOL {
     function supply(
